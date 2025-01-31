d0b0dab048c6fcba49b3dfdf050b8c8025b62bdf
Stephen Kariuki <stephenkariuki838@gmail.com>
2025-01-31 00:51:37 +0000

    test ci v 1

--- alacrity_backend/alacrity_backend/Test.py
+++ alacrity_backend/alacrity_backend/Test.py
@@ -1,6 +1,15 @@
 from django.test import TestCase
 from alacrity_backend.models import Test
 
+
+"""
+This file contains the tests for the models in the database. The tests are used to check if the models are working as expected
+this is a test model for the root applicstion
+it can be deleted
+
+
+"""
+
 class Test(TestCase):
     def test_example(self):
         self.assertEqual(1 + 1, 2)
