e20d18466d05837145fbee4939f3914a55782fbc
Stephen Kariuki <stephenkariuki838@gmail.com>
2025-02-03 22:28:19 +0000

    v3

--- alacrity_frontend/src/config.tsx
+++ alacrity_frontend/src/config.tsx
@@ -1,5 +1,5 @@
 
-// this is the config file for the frontend url   @/config.ts
+// this is the configc file for the frontend url   @/config.ts
 const BACKEND_URL = "http://127.0.0.1:8000/";
 
 export { BACKEND_URL };
