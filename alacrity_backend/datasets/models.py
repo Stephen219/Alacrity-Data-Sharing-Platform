57c8a165786d5cfa6ab89ef924ef2f6ddc2bdd6a
Clevis Gikenyi <gikenyic@cardiff.ac.uk>
2025-02-05 14:27:23 +0000

    added the view and the necessary urls , function that would handle data once it has been submitted into the system

--- alacrity_backend/datasets/models.py
+++ alacrity_backend/datasets/models.py
@@ -1,18 +1,25 @@
 from django.db import models
+from nanoid import generate
 
+
+def generate_id():
+    return generate(size=10)
 
 ## This is the model for the dataset table in the database holding the dataset information.
 class Dataset(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.TextField(max_length=10, primary_key=True, default=generate_id, editable=False)
    #orgid = models.ForeignKey(Organization, on_delete=models.CASCADE) this is the foreign key to the organization table it will be uncommented when the organization table is created
    #uploaderid = models.ForeignKey(User, on_delete=models.CASCADE) this is the foreign key to the user table it will be uncommented when the user table is created 
-    title = models.CharField(max_length=255)
-    category = models.CharField(max_length=255)
-    link = models.CharField(max_length=255)
-    description = models.TextField()
+    title = models.TextField(max_length=100, null=False)
+    category = models.TextField(max_length=255, null=False)
+    link = models.TextField(max_length=255, null=False)
+    description = models.TextField(null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
         return self.name
+
+
+# TODO add organisation and user id to the dataset
--- alacrity_backend/datasets/views.py
+++ alacrity_backend/datasets/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Dataset
+from urllib.parse import urlparse
+import json
 
+
+# this view creates the datases in the database, in future it will be updated to include the organization and user id by checking in the user who is logged in while making the request
+
+def is_valid_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc, result.path])  # Check if the url is valid
+    except:
+        return False
 # Create your views here.
+
+def create_dataset(request):
+    if request.method == 'POST':
+        # Get the data from the request
+        data = json.loads(request.body)
+
+        # extract the data from the request
+        title = data.get('title')
+        category = data.get('category')
+        link = data.get('link')
+        description = data.get('description')
+        # TODO: Add organization and user id to the dataset
+
+        # validating the data 
+        errors = {} # This will hold the errors
+
+        # Check if the title is empty
+        if not title:
+            errors['title'] = 'Title is required'
+        elif len(title) > 100:
+            errors['title'] = 'Title is too long'
+
+        # Check if the category is empty
+        valid_category = []
+        # TODO get the required categories from the database
+        if not category:
+            errors['category'] = 'Category is required'
+        elif category not in valid_category:
+            errors['category'] = f'Category must be one of the following: {", ".join(valid_category)}.'
+        
+        # validate the link
+        if not link:
+            errors['link'] = 'Link is required'
+        elif not is_valid_url(link):
+            errors['link'] = 'Invalid link'
+        
+        # validate the description
+        if not description:
+            errors['description'] = 'Description is required'
+        elif len(description) < 10:
+            errors['description'] = 'Description is too short'
+        elif len(description) > 255:
+            errors['description'] = 'Description is too long'
+        
+        # If there are errors, return the errors
+        if errors:
+            return JsonResponse(errors, status=400)
+        
+        # Create the dataset
+        dataset = Dataset(title=title, category=category, link=link, description=description)
+        dataset.save()
+        return JsonResponse({'message': 'Dataset created successfully'}, status=201)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
