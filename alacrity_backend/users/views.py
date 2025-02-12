bcd4ac13bac826aa0931305de194fcfd08452f03
Stephen Kariuki <kariukism1@cardiff.ac.uk>
2025-02-12 10:40:24 +0000

    Remove unused JWT authentication package and serializer import from views

--- alacrity_backend/users/views.py
+++ alacrity_backend/users/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from users.serializer import RegisterSerializer
+# from users.serializer import RegisterSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model, authenticate
