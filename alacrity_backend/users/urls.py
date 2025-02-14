69ab43c650a06cf5abc7ecad428a49f0bb171d41
Stephen Kariuki <kariukism1@cardiff.ac.uk>
2025-02-14 15:29:16 +0000

    cridentials

--- alacrity_backend/users/urls.py
+++ alacrity_backend/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import  RegisterView
 # from rest_framfework_simplejwt.views import TokenRefreshView
-# add login viejw
+# add login vhiejw
  
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
