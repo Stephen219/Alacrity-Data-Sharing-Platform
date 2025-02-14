ab708b30feeba0c2bf4137d5651aef20e6e85096
Stephen Kariuki <kariukism1@cardiff.ac.uk>
2025-02-14 14:10:00 +0000

    trial

--- alacrity_backend/users/urls.py
+++ alacrity_backend/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import  RegisterView
 # from rest_framfework_simplejwt.views import TokenRefreshView
+# add login view
  
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
