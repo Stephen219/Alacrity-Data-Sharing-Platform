07df4b1245f71c23819ed642d08e5eca3951038c
Clevis Gikenyi <gikenyic@cardiff.ac.uk>
2025-02-25 22:46:45 +0000

    Refactor contributor tests; improve code readability and organization, and remove commented-out code

--- alacrity_backend/organisation/urls.py
+++ alacrity_backend/organisation/urls.py
@@ -3,5 +3,4 @@ from .views import AddContributors
 
 urlpatterns = [
     path('add_contributor/', AddContributors.as_view(), name='add_contributor'),
-    #path('contributors/', GetContributors.as_view(), name='get_contributors'),
 ]
