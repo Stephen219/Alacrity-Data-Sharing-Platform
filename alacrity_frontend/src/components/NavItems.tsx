ce6f7e587b8bbd36414f8af09c9e7c5e70d466ca
Stephen Kariuki <kariukism1@cardiff.ac.uk>
2025-02-26 19:05:07 +0000

    Fix typo in UserRole type definition: change 'contributoR' to 'contributor'

--- alacrity_frontend/src/components/NavItems.tsx
+++ alacrity_frontend/src/components/NavItems.tsx
@@ -5,7 +5,7 @@ import { NAV_ITEMS } from "@/components/config/index";
 import NavItem from "./NavItem";
 import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
-export type UserRole = "organisation" | "researcher" |"contributoR" |null;
+export type UserRole = "organisation" | "researcher" |"contributor" |null;
 
 interface NavItemsProps {
   userRole: UserRole; 
