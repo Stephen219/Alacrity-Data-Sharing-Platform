c5980db5868d02b5ba16f1dd424f1a63987e4c57
Stephen Kariuki <kariukism1@cardiff.ac.uk>
2025-03-21 00:14:01 +0000

    add user dataset requests view for researchers

--- alacrity_backend/dataset_requests/urls.py
+++ alacrity_backend/dataset_requests/urls.py
@@ -3,13 +3,15 @@ from .views import (Make_request,ViewAllDatasetRequests,AcceptRejectRequest)
 
 from .views import (Make_request,ViewAllDatasetRequests)
 
-from .views import (Make_request,ViewAllDatasetRequests,AcceptRejectRequest)
+from .views import (Make_request,ViewAllDatasetRequests,AcceptRejectRequest, UserDatasetRequestsView)
 
 
 urlpatterns = [ 
     path('makerequest/', Make_request.as_view(), name='make_request'), 
     path('viewrequests/',ViewAllDatasetRequests.as_view(), name='view_requests'),
     path('acceptreject/<str:id>/', AcceptRejectRequest.as_view(), name='accept_reject'),
+    path('userrequests/', UserDatasetRequestsView.as_view(), name='user_requests'),
+
 
     # path('dataset/access/<str:dataset_id>/', ViewDatasetAccess.as_view(), name='dataset_access'),
 
