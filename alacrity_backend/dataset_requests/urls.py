18da8e18d3dbcd7d6d22118a30ac2b7b11003b18
Mahi Sheraj <sherajmm@cardiff.ac.uk>
2025-03-23 02:40:18 +0000

    url

--- alacrity_backend/dataset_requests/urls.py
+++ alacrity_backend/dataset_requests/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 
-from .views import (Make_request,View_pending,request_actions,view_requests)
+from .views import (Make_request, UserDatasetRequestsView,View_pending,request_actions,view_requests)
 
 
 urlpatterns = [ 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('viewpending/',View_pending.as_view(), name='view_requests'),
     path('requestaction/<str:id>/', request_actions.as_view(), name='request_actions'),
      path('viewrequests/', view_requests.as_view(), name='view_requests'),
+     path('userrequests/', UserDatasetRequestsView.as_view(), name='user-dataset-requests'),
     # path('dataset/access/<str:dataset_id>/', ViewDatasetAccess.as_view(), name='dataset_access'),
 ]
 
