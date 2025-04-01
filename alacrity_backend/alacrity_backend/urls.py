39ccd563a4ff1d8cc52bcb0d437cc5ccc407176a
Stephen Kariuki <kariukism1@cardiff.ac.uk>
2025-04-01 04:14:01 +0100

    Add health check endpoint and remove unused form submission code

--- alacrity_backend/alacrity_backend/urls.py
+++ alacrity_backend/alacrity_backend/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import health_check
 
 
 url = [
+    path("health/", health_check, name="health_check"),
     path('admin/', admin.site.urls),
     path('datasets/', include('datasets.urls')),
     path('organisation/', include('organisation.urls')),
