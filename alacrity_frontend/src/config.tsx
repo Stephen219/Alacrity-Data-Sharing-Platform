39c0a33c06e9e6b400a94b7e5ac470513afbe7bc
Stephen Kariuki <kariukism1@cardiff.ac.uk>
2025-04-03 14:26:00 +0100

    user soft delete

--- alacrity_frontend/src/config.tsx
+++ alacrity_frontend/src/config.tsx
@@ -1,7 +1,7 @@
 
 // this is the configc file for the frontend url   @/config.ts
-// const BACKEND_URL = "http://10.72.98.30/api";
-const BACKEND_URL = "http://localhost:8000";
+const BACKEND_URL = "http://10.72.98.30/api";
+// const BACKEND_URL = "http://localhost:8000";
 const MINIO_URL = "http:10.72.98.50:9000";
 
 export { BACKEND_URL, MINIO_URL };
